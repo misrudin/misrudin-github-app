a90ed12e4d994b6b01633069edf4232f2344bf58
misrudin <misrudinz@gmail.com>
2022-05-24 10:56:05 +0700

    add pagination

--- src/redux/slices/repository.ts
+++ src/redux/slices/repository.ts
@@ -3,10 +3,16 @@ import {apiGet} from "@configs/api";
 import {InitialState, ResponseApi} from "@interface/root";
 import {IRepository} from "@interface/repository";
 import {AppState} from "@redux/store";
+import {stat} from "fs";
 
-export const getRepositories = createAsyncThunk("repository/getRepositories", async (username?: string) => {
-    if(username) {
-        const response = await apiGet({url: `https://api.github.com/users/${username}/repos`}) as ResponseApi<IRepository[]>
+interface GetRepos {
+    username?: string
+    page?: number
+}
+
+export const getRepositories = createAsyncThunk("repository/getRepositories", async (params: GetRepos) => {
+    if (params.username) {
+        const response = await apiGet({url: `https://api.github.com/users/${params.username}/repos?per_page=20&page=${params.page}`}) as ResponseApi<IRepository[]>
         return response.data
     } else {
         return []
@@ -15,34 +21,49 @@ export const getRepositories = createAsyncThunk("repository/getRepositories", as
 
 interface IStateRepository extends InitialState {
     repositories?: IRepository[],
+    allowToFetch?: boolean
 }
 
-const initialState: IStateRepository = {}
+const initialState: IStateRepository = {
+    allowToFetch: true,
+}
 
 export const RepositorySlice = createSlice({
     name: 'repository',
     initialState,
-    reducers: {},
+    reducers: {
+        clearRepositories: (state, action) => {
+            state.repositories = undefined
+        }
+    },
     extraReducers: (builder) => {
         builder.addCase(getRepositories.pending, (state, action) => {
             state.is_loading = true
             state.is_error = false
+            state.allowToFetch = true
         })
-        builder.addCase(getRepositories.fulfilled, (state, {payload}) => {
-            state.repositories = payload
+        builder.addCase(getRepositories.fulfilled, (state, action: any) => {
+            const payload = action.payload
+            if (state.repositories && payload) {
+                state.repositories = [...state.repositories, ...payload]
+            } else {
+                state.repositories = payload
+            }
+
             state.is_loading = false
             if (payload && payload?.length > 0) {
                 state.msg = "Berhasil mendapatkan data repository."
                 state.is_error = false
             } else {
                 state.msg = "Repository tidak ditemukan."
-                state.is_error = true
+                state.allowToFetch = false
             }
         })
         builder.addCase(getRepositories.rejected, (state, action: any) => {
             state.is_loading = false
             state.is_error = true
             state.repositories = undefined
+            state.allowToFetch = false
 
             if (action.payload) {
                 state.msg = action.payload.message as string
@@ -53,5 +74,6 @@ export const RepositorySlice = createSlice({
     }
 })
 
+export const {clearRepositories} = RepositorySlice.actions
 export const selectRepositories = ((state: AppState) => state.repository)
 export default RepositorySlice.reducer
--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,6 +1,7 @@
 import moment from "moment";
 
-export function dateFormat(date: string): string {
+export function dateFormat(date?: string): string {
+    if(!date) return  ''
     return (
         date &&
         moment(date, 'YYYY-MM-DD HH:mm:ss').locale('id').format('DD MMM yyyy')
